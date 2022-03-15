300bb1f0e67e50cc9b7508caa53a884618e007bf
Akash More <akashkmore1@gmail.com>
2022-03-15 22:16:35 +0530

    Learn try catch in Async-Await

--- Asynchronous Javascript/Async-Await /Async-await.js
+++ Asynchronous Javascript/Async-Await /Async-await.js
@@ -10,12 +10,21 @@ const whereAmI = async (lat, lng) => {
   //       console.log(data);
   //     });
 
-  const response = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-  const responseData = await response.json();
-  console.log(responseData);
+  try {
+    const response = await fetch(
+      `https://geocode.xyz/${lat},${lng}?geoit=json`
+    );
+    if (!response.ok) {
+      throw new Error("Request sent before 3 seconds.");
+    }
+    const responseData = await response.json();
+    console.log(responseData);
 
-  // this Async function will return a promise.
-  return responseData;
+    // this Async function will return a promise.
+    return responseData;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 whereAmI(19.037, 72.873);
