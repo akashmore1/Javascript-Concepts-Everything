0a338b8eeb694de2b30c71913484140d69a14e8d
Akash More <akashkmore1@gmail.com>
2022-05-30 23:41:37 +0530

    Learn basics of strings

---
+++ Data-Structures-And-Logical-Operators-Strings/Working-With-Strings.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("Hello World!");
+
+const airLine = "TAP Air Portugal";
+const plane = "A230";
+
+// plane[0] should be 'A'
+console.log(plane[0]);
+console.log(plane[1]);
+
+// We can apply this directly on string like this:
+console.log("sdfsew"[4]);
+
+// To find length of string
+console.log(plane.length);
