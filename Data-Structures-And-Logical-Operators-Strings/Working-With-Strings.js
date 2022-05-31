65df845f5fee034998c91670b6834bc67984ef8e
Akash More <akashkmore1@gmail.com>
2022-05-31 23:30:29 +0530

    Learn slice method

--- Data-Structures-And-Logical-Operators-Strings/Working-With-Strings.js
+++ Data-Structures-And-Logical-Operators-Strings/Working-With-Strings.js
@@ -21,3 +21,14 @@ console.log(plane.indexOf("a")); // has O(n)
 console.log(plane.lastIndexOf("2"));
 // We can even find index of a word
 console.log(airLine.indexOf("Portugal"));
+
+// the slice method:
+// slice() method does not make changes in original array. I returns new array.
+const slicedString = airLine.slice(4);
+console.log(slicedString);
+// The second argument to slice method is index to which slicing should stop.
+// The second argument index does not included in new returned string.
+const slicedString2 = airLine.slice(4, 7);
+console.log(slicedString2);
+// We can give negative values in arguments of slice method.
+// .slice(-1) here char at index of -1 is last character.
