b098cae0624f382d3c9480489d40ca0be12c8d0b
Akash More <akashkmore1@gmail.com>
2022-05-31 23:21:12 +0530

    Learn indexOf and lastIndexOf

--- Data-Structures-And-Logical-Operators-Strings/Working-With-Strings.js
+++ Data-Structures-And-Logical-Operators-Strings/Working-With-Strings.js
@@ -3,7 +3,7 @@
 console.log("Hello World!");
 
 const airLine = "TAP Air Portugal";
-const plane = "A230";
+const plane = "A230a22";
 
 // plane[0] should be 'A'
 console.log(plane[0]);
@@ -14,3 +14,10 @@ console.log("sdfsew"[4]);
 
 // To find length of string
 console.log(plane.length);
+
+// To find index of certain letter in string
+console.log(plane.indexOf("a")); // has O(n)
+// To find last occurance of a char
+console.log(plane.lastIndexOf("2"));
+// We can even find index of a word
+console.log(airLine.indexOf("Portugal"));
