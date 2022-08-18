a2edc2551ad37120e26d6928edb1109fd97720e9
Akash More <akashkmore1@gmail.com>
2022-08-18 13:00:43 +0530

    Create object with constructor function

---
+++ Object-Oriented-Javascript/01-Constructor_function.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Every other language needs a class and constructor function to create an instance(object).
+// But in js we can create objects only with the help of constructor function.
+// constructor function is always named in capitals.
+
+const Person = function (name, id) {
+  this.name = name;
+  this.id = id;
+};
+
+const p1 = new Person("Akash", "1234");
+console.log(p1);
