60804e18e6af6671460b1c2fa5ea2ea259876699
Akash More <akashkmore1@gmail.com>
2022-08-18 16:49:52 +0530

    Learn about hasOwnProperty and prtotypal inheritance

--- Object-Oriented-Javascript/02-Prototypes.js
+++ Object-Oriented-Javascript/02-Prototypes.js
@@ -17,3 +17,24 @@ console.log(akash);
 console.log(akash.__proto__);
 console.log(akash.__proto__ === Person.prototype); // return true
 console.log(Person.prototype.isPrototypeOf(akash)); // return true
+
+// hasOwnProperty:
+// We can also add properties with the help of prototypes.
+
+const Car = function (color, company, price) {
+  this.color = color;
+  this.company = company;
+  this.price = price;
+};
+
+const aura = new Car("white", "Hyundai", 8.4);
+
+Car.prototype.calcOnRoadPrice = function () {
+  return this.price + 1;
+};
+
+Car.prototype.country = "Japan";
+
+console.log(aura);
+console.log(aura.hasOwnProperty("color")); // true
+console.log(aura.hasOwnProperty("country")); // false
