59b11b743e6a2cf3568b1f1e0e033874d9894df2
Akash More <akashkmore1@gmail.com>
2022-08-20 00:52:11 +0530

    at() method on strings

--- Working-With-Arrays/2-New_At_Methos.js
+++ Working-With-Arrays/2-New_At_Methos.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // at() is a new method es2022 feature
+// We can apply at() method on strings as well
 
 const arr = [1, 2, 3, 4];
 console.log(arr.at(2)); //return element at index 2
