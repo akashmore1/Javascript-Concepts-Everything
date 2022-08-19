7acce41408ac66ca4c390fb161301a2b3018f848
Akash More <akashkmore1@gmail.com>
2022-08-20 00:27:30 +0530

    Learn reverse, concat method

--- Working-With-Arrays/1-Simple_Array_Methods.js
+++ Working-With-Arrays/1-Simple_Array_Methods.js
@@ -20,3 +20,18 @@ arr.splice(2, 1); // This will remove 2nd index element
 // If we add 3rd argument in the array, that value will be added to index(1st argument)
 arr.splice(3, 0, 4); // number 4 will be added to 3rd index and 0 elements will be removed from array.
 // This method is very important while adding any element in middle of an array
+
+// reverse()
+// Important thing about this is reverse() method makes changes in original value
+const arr2 = ["a", "b", "c", "d"];
+arr2.reverse();
+console.log(arr2); // reversed array
+
+// concat()
+// This does not make any changes in original arrays
+const arr_1 = [1, 2, 3];
+const arr_2 = [4, 5, 6];
+const nums = arr_1.concat(arr_2); //[1,2,3,4,5,6]
+console.log(nums);
+// The above concat method is same as
+const nums1 = [...arr1, ...arr2];
