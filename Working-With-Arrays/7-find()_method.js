2980988a493f20ff211951c7d0414c6793242522
Akash More <akashkmore1@gmail.com>
2022-08-20 14:49:23 +0530

    Learn find method

---
+++ Working-With-Arrays/7-find()_method.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// find() method is same as filter method, it's callback functions return boolean.
+// The only difference is filter returns array of all elements satifying condition
+// find returns first value satisfying condition
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// find value greater than 3
+
+const greateThan4 = nums.find(function (num) {
+  return num > 3;
+});
