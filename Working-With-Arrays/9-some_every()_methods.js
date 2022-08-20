5cc59f7c48786731bb5c0d36d4dcacca2d4c567a
Akash More <akashkmore1@gmail.com>
2022-08-20 15:27:46 +0530

    Learn flat and flatmap

--- Working-With-Arrays/9-some_every()_methods.js
+++ Working-With-Arrays/9-some_every()_methods.js
@@ -12,3 +12,12 @@ const ifSquareIsPresent = arr.some(function (num) {
 // every()
 // every is similar as some return true or false
 // Its just some checks if any one element matches condition, while every checks if all values matches condition
+
+// ========================================================================================
+
+// flat() and flatMap()
+// if we have nested array and we want all elements in outside array, we use flat and flatMap
+const arrNest = [1, [2, 3], 4, [4, 6]];
+console.log(arrNest.flat()); // return [1,2,3,4,4,6]
+// But flat method only works on 1 level.
+// If array is deeply nested and we want above result, we use flatMap()
